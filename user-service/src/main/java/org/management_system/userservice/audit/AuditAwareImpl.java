1ca4e265bc9daaa76e5e7a6072b4148b06142d32
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-11-22 18:27:10 +0600

    update pom for all service

--- user-service/src/main/java/org/management_system/userservice/audit/AuditAwareImpl.java
+++ user-service/src/main/java/org/management_system/userservice/audit/AuditAwareImpl.java
@@ -1,21 +1,10 @@
 package org.management_system.userservice.audit;
 
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component("auditAwareImpl")
-public class AuditAwareImpl implements AuditorAware<String> {
-
-    /**
-     * Returns the current auditor of the application.
-     *
-     * @return the current auditor.
-     */
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.of("ACCOUNTS_MS");
-    }
+public class AuditAwareImpl {
 
 }
